25c6ec0892a677c60115ad857feb465ea57e11a0
valselee <sollleky72@gmail.com>
2023-10-05 15:14:29 +0900

    [init] 1. Project Init

---
+++ talklat/talklat/talklatApp.swift
@@ -0,0 +1,17 @@
+//
+//  talklatApp.swift
+//  talklat
+//
+//  Created by Celan on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct talklatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
