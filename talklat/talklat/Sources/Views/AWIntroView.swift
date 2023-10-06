3c9e30d1daf83c104cd891c89ac5c4a3cdd1131a
valselee <sollleky72@gmail.com>
2023-10-06 17:44:23 +0900

    [add] 1. AWIntroView 파일 생성

---
+++ talklat/talklat/Sources/Views/AWIntroView.swift
@@ -0,0 +1,20 @@
+//
+//  AWIntroView.swift
+//  talklat
+//
+//  Created by Celan on 2023/10/05.
+//
+
+import SwiftUI
+
+struct AWIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AWIntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        AWIntroView()
+    }
+}
