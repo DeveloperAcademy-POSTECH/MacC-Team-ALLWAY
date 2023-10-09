a58a8319c056fd7d2fcfc7c1e3607e74ca679ff8
valselee <sollleky72@gmail.com>
2023-10-09 23:08:42 +0900

    [Feat/#4] 1. Guide Message와 Placeholder를 상수형으로 정의

--- talklat/talklat/Sources/Utilities/Constants.swift
+++ talklat/talklat/Sources/Utilities/Constants.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+public enum Constants {
+    static let TEXTFIELD_PLACEHOLDER: String =
+    """
+    상대방에게 보여주고 싶은 내용을 작성해 주세요.
+    """
+    
+    static let GUIDE_MESSAGE: String =
+    """
+    저는 청각장애가 있어요.
+    음성 인식을 위해 천천히 크게 말해주세요.
+    """
+}
