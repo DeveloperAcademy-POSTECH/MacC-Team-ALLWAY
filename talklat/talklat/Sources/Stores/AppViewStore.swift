14fa13b3230707a98d814abe22608759db43240b
valselee <sollleky72@gmail.com>
2023-10-09 23:08:14 +0900

    [Feat/#4] 1. Store 외부의 액션을 받아오고 메소드에 따라 Published를 수정하도록 구현 2. 기본적인 동작을 위한 Text 와 상태 제어를 위한 enum 타입 정의 3. Preview 편의를 위해 instance를 입맛대로 정의할 수 있도록 함, 추후에 INIT과 별 차이 없으면 삭제 예정 4. custom Binding을 직접 구현해서 View가 아무런 State를 갖지 않도록 구현

--- talklat/talklat/Sources/Stores/AppViewStore.swift
+++ talklat/talklat/Sources/Stores/AppViewStore.swift
@@ -13,6 +13,63 @@ final class AppViewStore: ObservableObject {
         case writing
     }
     
-    @Published var communicationStatus: CommunicationStatus = .writing
-    @Published var questionText: String = ""
+    @Published private(set) var communicationStatus: CommunicationStatus
+    @Published private(set) var questionText: String
+    @Published private(set) var answeredText: String?
+    
+    public let questionTextLimit: Int = 55
+    
+    // MARK: INIT
+    init(
+        communicationStatus: CommunicationStatus,
+        questionText: String
+    ) {
+        self.communicationStatus = communicationStatus
+        self.questionText = questionText
+    }
+    
+    // MARK: HELPERS
+    public func enterSpeechRecognizeButtonTapped() {
+        withAnimation {
+            communicationStatus = .recording
+        }
+        
+    }
+    
+    public func stopSpeechRecognizeButtonTapped() {
+        withAnimation {
+            communicationStatus = .writing
+        }
+    }
+    
+    public func removeQuestionTextButtonTapped() {
+        questionText = ""
+    }
+    
+    public func bindingTextField(_ str: String) {
+        if str.count > questionTextLimit {
+            questionText = String(str.prefix(questionTextLimit))
+        } else {
+            questionText = str
+        }
+    }
+    
+    public func communicationStatusSetter(_ status: CommunicationStatus) {
+        communicationStatus = status
+    }
+    
+    public func questionTextSetter(_ str: String) {
+        questionText = str
+    }
+}
+
+// MARK: MAKE INSTANCES
+extension AppViewStore {
+    static func makePreviewStore(
+        condition: @escaping (AppViewStore) -> Void
+    ) -> AppViewStore {
+        let appViewStore = AppViewStore(communicationStatus: .writing, questionText: "")
+        condition(appViewStore)
+        return appViewStore
+    }
 }
