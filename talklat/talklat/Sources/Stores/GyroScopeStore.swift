40386c4a2af707e41adef01a2c7c9f691dd195ff
valselee <sollleky72@gmail.com>
2023-10-11 20:03:05 +0900

    [Update/#20] 1. AppViewStore의 일부 메소드가 Transition을 위한 withAnimation 클로저를 바디에 갖도록 수정 2. GyroScopeStore의 쓰지 않는 @Published var yaw 속성 제거 3. GyroScopeStore의  클로저 바디의 메소드를 분리

--- talklat/talklat/Sources/Stores/GyroScopeStore.swift
+++ talklat/talklat/Sources/Stores/GyroScopeStore.swift
@@ -10,18 +10,18 @@ import Foundation
 
 final class GyroScopeStore: ObservableObject {
     @Published private(set) var faced: FlippedStatus = .myself
-    @Published private(set) var yaw: Double = 0.0
     
     private let motionManager: CMMotionManager
     private let timeInterval: Double
     
+    // MARK: INIT
     init() {
         motionManager = CMMotionManager()
         timeInterval = 1.0 / 60.0
     }
     
     // Mark: Motion을 감지할때 사용하는 메서드
-    func startDeviceMotion() {
+    public func startDeviceMotion() {
         if motionManager.isDeviceMotionAvailable {
             self.motionManager.deviceMotionUpdateInterval = timeInterval
             self.motionManager.showsDeviceMovementDisplay = true
@@ -31,18 +31,8 @@ final class GyroScopeStore: ObservableObject {
             ) { (data, error) in
                 if let validData = data {
                     // 실제 coreMotion의 3가지 attitude 중 yaw로 flip 이벤트 측정
-                    self.yaw = abs(validData.attitude.yaw.toDegrees())
-                    
-                    self.faced = {
-                        switch self.yaw {
-                        case ...75:
-                            return FlippedStatus.myself
-                        case 75...:
-                            return FlippedStatus.opponent
-                        default:
-                            return FlippedStatus.myself
-                        }
-                    }()
+                    let yaw = abs(validData.attitude.yaw.toDegrees())
+                    self.faced = self.motionStatusSetter(yaw)
                 }
             }
         } else {
@@ -54,7 +44,18 @@ final class GyroScopeStore: ObservableObject {
     }
     
     // Mark: MotionManger를 멈출때 쓰는 메서드
-    func stopMotionManager() {
+    public func stopMotionManager() {
         self.motionManager.stopDeviceMotionUpdates()
+    }
+    
+    private func motionStatusSetter(_ yaw: Double) -> FlippedStatus {
+        switch yaw {
+        case ...75:
+            return FlippedStatus.myself
+        case 75...:
+            return FlippedStatus.opponent
+        default:
+            return FlippedStatus.myself
+        }
     }
 }
