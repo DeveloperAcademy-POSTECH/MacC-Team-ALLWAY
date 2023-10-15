b70ef49d167c8a0fcfac1cd874cf33ee8797086b
MADElinessss <jyseen@naver.com>
2023-10-16 02:20:33 +0900

    [Feat/#17] TextField DesignSystem 나머지 구현

--- talklat/talklat/Sources/Views/Components/TLTextField.swift
+++ talklat/talklat/Sources/Views/Components/TLTextField.swift
@@ -18,7 +18,7 @@ struct TLTextField<Button: View>: View {
     private var style: TLTextFieldStyle
     private var placeholder: String
     private var eraseAllButton: Button
-    
+    private let defaultTextPlaceholder: String = "상대방에게 보여주고 싶은 내용을 작성해 주세요."
     
     init(
         style: TLTextFieldStyle = .normal(textLimit: 55),
@@ -31,59 +31,74 @@ struct TLTextField<Button: View>: View {
         self.placeholder = placeholder
         self.eraseAllButton = eraseAllButton()
     }
+    
     var body: some View {
         switch style {
         case let .normal(textLimit):
             VStack {
+                eraseAllButtonSection
+                textCountIndicator(textLimit: textLimit)
+                inputFieldSection(textLimit: textLimit)
+            }
+        }
+    }
+    
+    private var eraseAllButtonSection: some View {
+        HStack {
+            eraseAllButton
+                .padding(.leading, 24)
+            Spacer()
+        }
+        .padding(.bottom, 24)
+    }
+    
+    private func textCountIndicator(textLimit: Int) -> some View {
+        HStack {
+            Text("\($text.wrappedValue.count)/\(textLimit)")
+                .monospacedDigit()
+                .foregroundColor(
+                    text.count >= textLimit
+                    ? .red
+                    : .gray
+                )
+                .padding(.leading, 24)
+            Spacer()
+        }
+    }
+    
+    private func inputFieldSection(textLimit: Int) -> some View {
+        ZStack {
+            if text.isEmpty {
                 HStack {
-                    eraseAllButton
-                        .padding(.leading, 24)
-                    Spacer()
-                }
-                .padding(.bottom, 24)
-                HStack {
-                    Text("\($text.wrappedValue.count)/\(textLimit)")
-                        .monospacedDigit()
-                        .foregroundColor(
-                            text.count >= textLimit
-                            ? .red
-                            : .gray
-                        )
-                        .padding(.leading, 24)
-                    Spacer()
-                }
-                ZStack {
-                    HStack {
-                        Text(text.count == 0 ? "상대방에게 보여주고 싶은 내용을 작성해 주세요." : "")
-                            .font(.system(size: 24, weight: .bold))
-                            .foregroundColor(.gray)
-                            .lineSpacing(24 * 0.8)
-                            .padding(.leading, 24)
-                            .padding(.bottom, 48)
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        TextField(
-                            placeholder,
-                            text: $text
-                        )
+                    Text(defaultTextPlaceholder)
                         .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.gray)
+                        .lineSpacing(19.2)
                         .padding(.leading, 24)
-                        .padding(.bottom, 96)
-                        .frame(maxWidth: .infinity)
-                        .autocorrectionDisabled()
-                        .textInputAutocapitalization(.never)
-                        .onChange(of: text) { _ in
-                            if text.count > textLimit {
-                                text = String(text.prefix(textLimit))
-                                isLetterExceeded = true
-                            }
-                            else {
-                                isLetterExceeded = false
-                            }
-                        }
-                    }
+                        .padding(.bottom, 48)
+                    Spacer()
+                }
+            }
+            
+            TextField(
+                placeholder,
+                text: $text
+//                axis: .vertical
+            )
+            .font(.system(size: 24, weight: .bold))
+            .padding(.leading, 24)
+            .padding(.trailing, 24)
+            .padding(.bottom, 96)
+            .lineSpacing(10)
+            .frame(maxWidth: .infinity)
+            .autocorrectionDisabled()
+            .textInputAutocapitalization(.never)
+            .onChange(of: text) { _ in
+                if text.count > textLimit {
+                    text = String(text.prefix(textLimit))
+                    isLetterExceeded = true
+                } else {
+                    isLetterExceeded = false
                 }
             }
         }
