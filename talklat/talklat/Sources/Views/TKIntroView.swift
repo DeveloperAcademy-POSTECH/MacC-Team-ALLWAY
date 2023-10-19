9fd86105d6a411aeebbb0e55a78093b0518050f5
valselee <sollleky72@gmail.com>
2023-10-19 10:06:42 +0900

    [Update/#38] 1. 설정에서 다크모드로 수정하고 돌아올 때, Color의 ColorScheme을 새롭게 할당하여 색변화에 대응

--- talklat/talklat/Sources/Views/TKIntroView.swift
+++ talklat/talklat/Sources/Views/TKIntroView.swift
@@ -10,6 +10,8 @@
 import SwiftUI
 
 struct TKIntroView: View {
+    @Environment(\.scenePhase)
+    var scenePhase
     @StateObject var gyroMotionStore: GyroScopeStore = GyroScopeStore()
     @ObservedObject var appViewStore: AppViewStore
     
@@ -39,6 +41,9 @@ struct TKIntroView: View {
                 }
                 HapticManager.sharedInstance.generateHaptic(.success)
             }
+        }
+        .onChange(of: scenePhase) { _ in
+            Color.colorScheme = UITraitCollection.current.userInterfaceStyle
         }
     }
 }
