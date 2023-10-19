891be5f3f1303a7017fd711e7ecf3df60ae4419b
valselee <sollleky72@gmail.com>
2023-10-19 10:07:06 +0900

    [Feat/#38] 1. Color를 Hex String으로 초기화할 수 있는 확장 구현

--- talklat/talklat/Sources/Utilities/Extensions/Color+Hex.swift
+++ talklat/talklat/Sources/Utilities/Extensions/Color+Hex.swift
@@ -5,4 +5,27 @@
 //  Created by Celan on 2023/10/18.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+        
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+    
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+}
