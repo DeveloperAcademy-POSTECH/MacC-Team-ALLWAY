a79ca02baf9a18337e65f7611a252c62aaa22499
valselee <sollleky72@gmail.com>
2023-10-31 14:56:58 +0900

    [Feat/#69] 1. TKGuidingView 구현 2. Circle Trim Progress Animation 3. 색상은 아직 미정

--- talklat/talklat/Sources/Views/TKGuidingView.swift
+++ talklat/talklat/Sources/Views/TKGuidingView.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct TKGuidingView: View {
+    @State private var circleTrim: CGFloat = 0.0
+    
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    let guide: String =
+    """
+    이 앱은 당신이 말하는 것을
+    제가 들을 수 있도록 도와줄 거예요.
+    
+    이 화면이 꺼지면, 제 질문을 읽고
+    크고 또박또박 말씀해주시기를
+    부탁드릴게요.
+    """
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Button {
+                
+            } label: {
+                Text("취소")
+                    .font(.title2)
+                    .bold()
+            }
+            
+            Spacer()
+                .frame(maxHeight: 60)
+            
+            Text("안녕하세요.\n저는 청각장애를\n가지고 있습니다.")
+                .font(.largeTitle)
+                .bold()
+                .lineSpacing(10)
+                .multilineTextAlignment(.leading)
+            
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: 2)
+                .padding(.top, 20)
+                .padding(.bottom, 40)
+            
+            Text(guide)
+                .font(.title3)
+                .bold()
+            
+            Spacer()
+            
+            HStack {
+                Circle()
+                    .trim(from: 0, to: circleTrim)
+                    .stroke(style: StrokeStyle(lineWidth: 4, lineCap: .round))
+                    .frame(width: 60, height: 60)
+                    .rotationEffect(.degrees(-90))
+            }
+            .frame(
+                maxWidth: .infinity,
+                alignment: .center
+            )
+        }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity
+        )
+        .foregroundColor(.white)
+        .padding(.horizontal, 24)
+        .background { Color.orange.ignoresSafeArea() }
+        .onReceive(timer) { _ in
+            withAnimation {
+                if circleTrim <= 1 {
+                    circleTrim += 0.05
+                    print(circleTrim)
+                } else if circleTrim >= 1 {
+                    print("Start Recording")
+                    timer.upstream.connect().cancel()
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    TKGuidingView()
+struct TKGuidingView_Preview: PreviewProvider {
+    static var previews: some View {
+        TKGuidingView()
+    }
 }
