9d6521e66b3f9ecbe843a6bd95b6769ec4b40631
valselee <sollleky72@gmail.com>
2023-10-31 14:55:13 +0900

    [Add/#69] 1. TKGuidingView가 생성됨

---
+++ talklat/talklat/Sources/Views/TKGuidingView.swift
@@ -0,0 +1,18 @@
+//
+//  TKGuidingView.swift
+//  talklat
+//
+//  Created by Celan on 10/31/23.
+//
+
+import SwiftUI
+
+struct TKGuidingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TKGuidingView()
+}
