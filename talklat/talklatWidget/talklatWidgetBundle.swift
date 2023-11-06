a4c28bfb3b86f183d8985b2d9a83ec91d21323d4
valselee <sollleky72@gmail.com>
2023-11-06 21:20:26 +0900

    [Target/#83] 1. Widget Target 추가 2. Widget 기본 UI 구현 완료! 3. 추후 위치 기반 기능 기획이 완료되면 추가 이슈를 열어서 작업 예정

---
+++ talklat/talklatWidget/talklatWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  talklatWidgetBundle.swift
+//  talklatWidget
+//
+//  Created by Celan on 11/6/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct talklatWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        talklatWidget()
+        talklatWidgetLiveActivity()
+    }
+}
