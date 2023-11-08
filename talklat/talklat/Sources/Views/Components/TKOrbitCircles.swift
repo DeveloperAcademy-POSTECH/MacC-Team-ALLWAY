a48daa5345380bb62d69e5b3258518088229e523
valselee <sollleky72@gmail.com>
2023-11-08 21:37:51 +0900

    [Feat/#92] 1. Converstaion을 시작할 때, 버튼 뒤에서 동작하는 OrbitalCircles 구현

--- talklat/talklat/Sources/Views/Components/TKOrbitCircles.swift
+++ talklat/talklat/Sources/Views/Components/TKOrbitCircles.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct TKOrbitCircles: View {
+    struct CircleOffset: Hashable {
+        let x: CGFloat
+        let y: CGFloat
+    }
+    
+    @State private var isAnimating: Bool = false
+    
+    private let startingOffset: [CircleOffset] = [
+        CircleOffset(x: 37, y: -5),
+        CircleOffset(x: -11, y: 42),
+        CircleOffset(x: -30, y: -21),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ForEach(
+                startingOffset,
+                id: \.self
+            ) { offset in
+                Group {
+                    Circle()
+                        .frame(
+                            width: proxy.size.width / 2,
+                            height: proxy.size.height / 2,
+                            alignment: .center
+                        )
+                        .offset(
+                            x: offset.x,
+                            y: offset.y
+                        )
+                        .foregroundStyle(Color.accentColor.opacity(0.3))
+                }
+                .frame(
+                    width: proxy.size.width,
+                    height: proxy.size.height
+                )
+                .rotationEffect(
+                    self.isAnimating ? .degrees(360) : .zero
+                )
+                .animation(
+                    Animation
+                        .linear(duration: 30.0)
+                        .repeatForever(autoreverses: false),
+                    value: isAnimating
+                )
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .onAppear {
+            isAnimating = true
+        }
     }
 }
 
