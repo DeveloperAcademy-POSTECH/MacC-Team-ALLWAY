127389b2e0183651930c2939c2b3d29d22b81171
valselee <sollleky72@gmail.com>
2023-11-09 23:25:26 +0900

    [Feat/#92] 1. 피그마에서 지정된 사이즈와 opacity를 기반으로 Circles View 구현 2. opacity 애니메이션 구현

--- talklat/talklat/Sources/Views/Components/TKBreathingCircles.swift
+++ talklat/talklat/Sources/Views/Components/TKBreathingCircles.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct TKBreathingCircles: View {
+    struct BreathingCircleRender: Hashable {
+        let frame: CGFloat
+        let opacity: CGFloat
+    }
+    
+    let breathingCircles: [BreathingCircleRender] = [
+        BreathingCircleRender(frame: 240, opacity: 0.4),
+        BreathingCircleRender(frame: 200, opacity: 0.6),
+        BreathingCircleRender(frame: 160, opacity: 1.0),
+    ]
+    
+    @State private var animationFlag: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ForEach(breathingCircles, id: \.self) { circle in
+                Circle()
+                    .foregroundStyle(Color.white)
+                    .opacity(
+                        animationFlag && circle.opacity != 1.0
+                        ? circle.opacity / 2
+                        : circle.opacity
+                    )
+                    .frame(
+                        maxWidth: circle.frame,
+                        maxHeight: circle.frame
+                    )
+                    .animation(
+                        .easeInOut
+                            .speed(0.2)
+                            .repeatForever(autoreverses: true),
+                        value: animationFlag
+                    )
+            }
+            .frame(
+                maxWidth: proxy.size.width,
+                maxHeight: proxy.size.height,
+                alignment: .center
+            )
+        }
+        .onAppear {
+            animationFlag.toggle()
+        }
     }
 }
 
