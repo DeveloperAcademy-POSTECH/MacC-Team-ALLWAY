47c46ddb66d927926bb285b1b09562fb9bd1de93
valselee <sollleky72@gmail.com>
2023-11-09 17:40:49 +0900

    [Add/#92] 1. TKBreathingCircles 파일 생성

---
+++ talklat/talklat/Sources/Views/Components/TKBreathingCircles.swift
@@ -0,0 +1,18 @@
+//
+//  TKBreathingCircles.swift
+//  talklat
+//
+//  Created by Celan on 11/9/23.
+//
+
+import SwiftUI
+
+struct TKBreathingCircles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TKBreathingCircles()
+}
