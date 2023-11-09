e2d6ab21bf7b3d197f50b4e66dcef64c5ef8dfd5
valselee <sollleky72@gmail.com>
2023-11-09 23:24:14 +0900

    [Feat/#92] 1. TKOrbitalCircles -> TKBreathingCircles 로 애니메이션 뷰 수정 2. 피그마 기반 색상으로 수정

--- talklat/talklat/Sources/Views/Main/TKMainView.swift
+++ talklat/talklat/Sources/Views/Main/TKMainView.swift
@@ -12,28 +12,42 @@ struct TKMainView: View {
     
     var body: some View {
         ZStack {
-            VStack {
-                #warning("LOCATION HERE")
+            VStack(spacing: 0) {
+                Spacer()
+                    .frame(maxHeight: 120)
                 
-                TKOrbitCircles()
-                    .overlay {
-                        NavigationLink {
-                            Text("?")
-                        } label: {
-                            Circle()
-                                .opacity(0.5)
-                                .frame(width: 195, height: 195)
-                                .overlay {
-                                    Text(Constants.START_CONVERSATION_MESSAGE)
-                                        .multilineTextAlignment(.center)
-                                        .font(.title)
-                                        .bold()
-                                        .foregroundStyle(.white)
-                                }
-                        }
+                HStack(spacing: 2) {
+                    Image(systemName: "location.fill")
+                    
+                    Text("${CURRENT_LOCATION}")
+                }
+                .font(.caption)
+                .bold()
+                .foregroundStyle(Color.gray)
+                .padding(.bottom, 10)
+                
+                Text("새 대화 시작하기")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(Color.white)
+                    
+                ZStack {
+                    TKBreathingCircles()
+                    
+                    NavigationLink {
+                        Text("?")
+                    } label: {
+                        Image(systemName: "ellipsis.message.fill")
+                            .resizable()
+                            .frame(maxWidth: 86, maxHeight: 77)
                     }
+                }
+                .aspectRatio(contentMode: .fit)
+                .frame(maxHeight: 250)
+                .padding(.top, 32)
             }
             .frame(
+                maxWidth: .infinity,
                 maxHeight: .infinity,
                 alignment: .top
             )
@@ -41,11 +55,13 @@ struct TKMainView: View {
             // MARK: BottomSheet
             TKDraggableList(store: store)
         }
+        .background { Color.accentColor.ignoresSafeArea(edges: .top) }
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Text("TALKLAT")
                     .font(.title)
                     .bold()
+                    .foregroundStyle(Color.white)
             }
             
             ToolbarItem(placement: .topBarTrailing) {
@@ -53,7 +69,7 @@ struct TKMainView: View {
                     Text("SETTING VIEW HERE")
                 } label: {
                     Image(systemName: "gearshape.fill")
-                        .foregroundStyle(Color.gray400)
+                        .foregroundStyle(Color.white)
                 }
             }
             
@@ -62,7 +78,7 @@ struct TKMainView: View {
                     Text("ALL HISTORY VIEW HERE")
                 } label: {
                     Image(systemName: "list.dash")
-                        .foregroundStyle(Color.gray400)
+                        .foregroundStyle(Color.white)
                 }
             }
         }
