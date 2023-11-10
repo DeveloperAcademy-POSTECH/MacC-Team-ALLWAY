8eb9d2ba70b854cff2ca87277e8630eb41d7b617
valselee <sollleky72@gmail.com>
2023-11-10 10:52:26 +0900

    [Feat/#92] 1. TKMainStore 구현 2. TKReducer를 채택 3. Drag와 관련된 State를 관리하는 ViewState 선언 및 정의

--- talklat/talklat/Sources/Stores/TKMainViewStore.swift
+++ talklat/talklat/Sources/Stores/TKMainViewStore.swift
@@ -5,4 +5,50 @@
 //  Created by Celan on 11/8/23.
 //
 
-import Foundation
+import SwiftUI
+
+final class TKMainViewStore {
+    struct ViewState {
+        var isBottomSheetMaxed: Bool = false
+        var offset: CGFloat = 0
+        var lastOffset: CGFloat = 0
+    }
+    
+    @Published private var viewState: ViewState = ViewState()
+    
+    public func onBottomSheetMaxed(_ height: CGFloat) {
+        let flag = height <= abs(self(\.lastOffset))
+        reduce(\.isBottomSheetMaxed, into: flag)
+    }
+    
+    public func onUpdatingDragOffset(_ gestureOffset: CGFloat) {
+        Task { @MainActor in
+            reduce(\.offset, into: gestureOffset + self(\.lastOffset))
+        }
+    }
+    
+    public func onDragEnded(_ maxHeight: CGFloat) {
+        withAnimation(.spring()) {
+            if -self(\.offset) > maxHeight / 2 {
+                reduce(\.offset, into: -maxHeight)
+            } else {
+                reduce(\.offset, into: 0)
+            }
+        }
+        reduce(\.lastOffset, into: self(\.offset))
+    }
+}
+
+extension TKMainViewStore: TKReducer {
+    func callAsFunction<Value: Equatable>
+    (_ path: KeyPath<ViewState, Value>) -> Value {
+        self.viewState[keyPath: path]
+    }
+    
+    func reduce<Value: Equatable>(
+        _ path: WritableKeyPath<ViewState, Value>,
+        into newValue: Value
+    ) {
+        viewState[keyPath: path] = newValue
+    }
+}
