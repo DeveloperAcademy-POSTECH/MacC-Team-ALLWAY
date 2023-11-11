6a6e85f0e5e2ee9dd3bba2646577875b62839077
valselee <sollleky72@gmail.com>
2023-11-12 02:17:39 +0900

    [Feat/#99] 1. Transition을 각 View 의 위치에 구애받지 않고 mathed Effect를 할 수 있도록 environment 구현

--- talklat/talklat/Sources/Utilities/Extensions/EnvironmentValues+Namespace.swift
+++ talklat/talklat/Sources/Utilities/Extensions/EnvironmentValues+Namespace.swift
@@ -5,4 +5,21 @@
 //  Created by Celan on 11/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct NamespaceEnvironmentKey: EnvironmentKey {
+    static var defaultValue: Namespace.ID = Namespace().wrappedValue
+}
+
+extension EnvironmentValues {
+    var namespace: Namespace.ID {
+        get { self[NamespaceEnvironmentKey.self] }
+        set { self[NamespaceEnvironmentKey.self] = newValue }
+    }
+}
+
+extension View {
+    func publishNamespace(_ value: Namespace.ID) -> some View {
+        environment(\.namespace, value)
+    }
+}
