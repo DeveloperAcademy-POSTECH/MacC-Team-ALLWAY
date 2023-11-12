0351a40bfeb34b61e33f2dd552cace3ce2122fb8
valselee <sollleky72@gmail.com>
2023-11-12 23:19:12 +0900

    [Add/#102] 1. TKScrollView 파일 생성

---
+++ talklat/talklat/Sources/Views/Components/TKScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  TKScrollView.swift
+//  talklat
+//
+//  Created by Celan on 11/12/23.
+//
+
+import SwiftUI
+
+struct TKScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TKScrollView()
+}
