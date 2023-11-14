6d7ec9b6681f0aec909f1261cd15d841169fdd66
valselee <sollleky72@gmail.com>
2023-11-14 22:17:05 +0900

    [Feat/#114] 1. TKToast 구현 2. binding 받은 속성을 2초 후 자동으로 토글하여 dismiss 3. top insertion, top removal transition 적용

--- talklat/talklat/Sources/Views/Components/TKToast.swift
+++ talklat/talklat/Sources/Views/Components/TKToast.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct TKToast: View {
+    @Binding var flag: Bool
+    let text: String = "Location"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if flag {
+            HStack(spacing: 12) {
+                Image(systemName: "checkmark.circle.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(
+                        Color.white,
+                        Color.OR5
+                    )
+                    .padding(.leading, 16)
+                
+                VStack(alignment: .leading) {
+                    Text("TITLE")
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                    
+                    HStack(spacing: 4) {
+                        Image(systemName: "location.fill")
+                        
+                        Text("\(text)")
+                        +
+                        Text("에 대화가 저장되었어요.")
+                    }
+                    .font(.caption2)
+                    .foregroundStyle(Color.gray400)
+                }
+            }
+            .padding(.horizontal, 16)
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: 65,
+                alignment: .leading
+            )
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color.gray700)
+                    .padding(.horizontal, 16)
+            }
+            .transition(
+                .move(edge: .top)
+                .combined(with: .opacity)
+            )
+            .task {
+                try? await Task.sleep(for: .seconds(2.0))
+                withAnimation {
+                    flag.toggle()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    TKToast()
+    TKToast(flag: .constant(false))
 }
