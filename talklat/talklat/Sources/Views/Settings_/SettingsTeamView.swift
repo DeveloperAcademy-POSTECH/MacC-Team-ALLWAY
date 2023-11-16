6a6ebb98c23d7aefc52b358a09cb98fb6e72078f
MADElinessss <jyseen@naver.com>
2023-11-16 15:29:18 +0900

    [Feat/#134] TeamView 생성

---
+++ talklat/talklat/Sources/Views/Settings_/SettingsTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTeamView.swift
+//  talklat
+//
+//  Created by 신정연 on 11/16/23.
+//
+
+import SwiftUI
+
+struct SettingsTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsTeamView()
+}
