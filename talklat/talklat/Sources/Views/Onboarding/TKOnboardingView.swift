f5f30a4ac1cd64a8afb09bd3fd5a169b6455e54c
valselee <sollleky72@gmail.com>
2023-11-16 15:53:19 +0900

    [Feat/#131] 1. TKOnboardingView 의 UI 구현 2. Animation & Transition 적용 완료 3. Logic 연결 예정

--- talklat/talklat/Sources/Views/Onboarding/TKOnboardingView.swift
+++ talklat/talklat/Sources/Views/Onboarding/TKOnboardingView.swift
@@ -36,21 +36,21 @@ struct TKOnboardingView: View {
             }
             
             if case let .mic(info) = onboardingStep {
-                TKRequestAuthorizationView(
+                TKOnboardingAuthReqeustView(
                     parentInfo: $onboardInfo,
                     info: info
                 )
             }
             
             if case let .speech(info) = onboardingStep {
-                TKRequestAuthorizationView(
+                TKOnboardingAuthReqeustView(
                     parentInfo: $onboardInfo,
                     info: info
                 )
             }
             
             if case let .location(info) = onboardingStep {
-                TKRequestAuthorizationView(
+                TKOnboardingAuthReqeustView(
                     parentInfo: $onboardInfo,
                     info: info
                 )
@@ -241,49 +241,4 @@ struct TKOnboardingView: View {
 
 #Preview {
     TKOnboardingView()
-}
-
-struct TKRequestAuthorizationView: View {
-    @Binding var parentInfo: TKOnboardingView.OnboardingInfo
-    let info: TKOnboardingView.OnboardingInfo
-    
-    var body: some View {
-        VStack(
-            alignment: .leading,
-            spacing: 0
-        ) {
-            HStack(spacing: 15) {
-                Circle()
-                    .frame(width: 34, height: 34)
-                    
-                Text("\(info.title).")
-                    .font(.largeTitle)
-                    .bold()
-            }
-            .foregroundStyle(Color.OR6)
-            .padding(.bottom, 36)
-            
-            Text("\(info.description)")
-                .font(.title2)
-                .bold()
-                .multilineTextAlignment(.leading)
-                .lineSpacing(16)
-            
-            Spacer()
-        }
-        .frame(
-            maxWidth: .infinity,
-            maxHeight: .infinity,
-            alignment: .topLeading
-        )
-        .transition(
-            .asymmetric(
-                insertion: .move(edge: .trailing),
-                removal: .move(edge: .leading)
-            )
-        )
-        .onAppear {
-            self.parentInfo = info
-        }
-    }
 }
