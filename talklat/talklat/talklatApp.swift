a07c3a3aa57686af3f42aedc4d595e801286a581
lianne-b <yenchoi010@gmail.com>
2023-11-16 16:46:47 +0900

    [Fix] 1. EnvironmentObject 초기화 이슈 수정

--- talklat/talklat/talklatApp.swift
+++ talklat/talklat/talklatApp.swift
@@ -12,9 +12,7 @@ import SwiftData
 struct talklatApp: App {
     @Environment(\.scenePhase) private var scenePhase
     @StateObject private var store: TKMainViewStore = TKMainViewStore()
-//    @StateObject var locationStore: LocationStore = LocationStore()
     private var container: ModelContainer
-
     
     init() {
         do {
@@ -34,13 +32,12 @@ struct talklatApp: App {
             Group {
                 switch store(\.authStatus) {
                 case .splash:
-                    AuthTestingView()
-//                    TKSplashView()
-//                        .task {
-//                            try? await Task.sleep(for: .seconds(3.0))
-//                            let status = await SpeechAuthManager.switchAuthStatus()
-//                            store.onVoiceAuthorizationObtained(status)
-//                        }
+                    TKSplashView()
+                        .task {
+                            try? await Task.sleep(for: .seconds(3.0))
+                            let status = await SpeechAuthManager.switchAuthStatus()
+                            store.onVoiceAuthorizationObtained(status)
+                        }
 
                 default:
                     NavigationStack {
@@ -49,7 +46,6 @@ struct talklatApp: App {
                     .transition(.opacity.animation(.easeInOut))
                 }
             }
-//            .environmentObject(locationStore)
             .onChange(of: scenePhase) { _, _ in
                 Color.colorScheme = UITraitCollection.current.userInterfaceStyle
             }
