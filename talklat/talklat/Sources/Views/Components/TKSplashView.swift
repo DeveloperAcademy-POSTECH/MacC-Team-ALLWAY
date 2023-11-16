ab42068ee0daa0ef0afa519da82be1abb2a5ad5c
valselee <sollleky72@gmail.com>
2023-11-16 10:35:58 +0900

    [Add/#126] 1. SplashView 파일 생성 2. Asset 파일 추가 3. AppIcon 변경

---
+++ talklat/talklat/Sources/Views/Components/TKSplashView.swift
@@ -0,0 +1,18 @@
+//
+//  TKSplashView.swift
+//  talklat
+//
+//  Created by Celan on 11/15/23.
+//
+
+import SwiftUI
+
+struct TKSplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TKSplashView()
+}
