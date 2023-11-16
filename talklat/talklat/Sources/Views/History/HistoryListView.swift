e3f03e1b13e87f8b2e309a446ab14efd0f2535e4
lianne-b <yenchoi010@gmail.com>
2023-11-16 17:42:49 +0900

    [Fix/#132] 히스토리 서치 뷰 - 네비게이션 연결

--- talklat/talklat/Sources/Views/History/HistoryListView.swift
+++ talklat/talklat/Sources/Views/History/HistoryListView.swift
@@ -66,6 +66,7 @@ struct HistoryListView: View {
                                 isEditing: $isEditing,
                                 isDialogShowing: $isDialogShowing
                             )
+                            .padding(.bottom, 24)
                         }
                         .padding(.top, 24)
                     }
@@ -223,7 +224,7 @@ struct LocationList: View {
                 ForEach(
                     dataStore.getLocationBasedConversations(location: location),
                     id: \.self
-                ) { conversation in // TODO: each TKLocation의 TKConversation
+                ) { conversation in
                     NavigationLink {
                         CustomHistoryView(
                             historyViewType: .item,
@@ -386,7 +387,7 @@ struct CustomDialog: View {
                     .foregroundColor(.GR9)
                     .font(.system(size: 17, weight: .bold))
                 
-                Text("..에서 저장된\n모든 데이터가 삭제됩니다.") // TODO: TKConversation.title
+                Text("\"\(selectedConversation.title)\"에서 저장된\n모든 데이터가 삭제됩니다.")
                     .multilineTextAlignment(.center)
                     .foregroundColor(.GR6)
                     .font(.system(size: 15, weight: .medium))
@@ -424,11 +425,6 @@ struct CustomDialog: View {
         .cornerRadius(22)
         .frame(height: 240)
         .frame(maxWidth: .infinity)
-    }
-    
-    func removeConversation(_ id: String) {
-        // TODO: Delete하는 로직 (String -> PersistentIdentifier를 이용해 객체를 특정)
-        
     }
 }
 
