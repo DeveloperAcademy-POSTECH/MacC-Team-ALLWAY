5ce7a4f1f9d69c570d25c26177752a368663bd12
MADElinessss <jyseen@naver.com>
2023-11-17 11:32:50 +0900

    [Feat/#134] TeamView 끝

--- talklat/talklat/Sources/Views/Settings_/SettingsTeamView.swift
+++ talklat/talklat/Sources/Views/Settings_/SettingsTeamView.swift
@@ -8,41 +8,110 @@
 import SwiftUI
 
 struct SettingsTeamView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    HStack {
-                        Image("TeamLogo")
-                        Spacer()
-                    }
-                    .padding(.leading, 24)
-                    .padding(.top, 24)
-                    .padding(.bottom, 16)
-                    HStack {
-                        Text("올웨이팀은 어쩌구저쩌구 입니다.\n어쩌구저쩌구해서 머하는 팀입니다.")
-                            .font(.system(size: 17, weight: .bold))
-                            .lineSpacing(17 * 1.6 - 17)
-                            .padding(.vertical, (17 * 1.6 - 17) / 2)
-                        
-                        Spacer()
-                    }
-                    .padding(.leading, 24)
-                    .padding(.bottom, 50)
+        ScrollView {
+            VStack {
+                HStack {
+                    Image("TeamLogo")
+                    Spacer()
+                }
+                .padding(.leading, 24)
+                .padding(.top, 24)
+                .padding(.bottom, 16)
+                HStack {
+                    Text("애플 디벨로퍼 아카데미 수료생으로 구성된\nALLWAY(올해의 팀) 팀은 사회의 다양한 문제를\n탐구하고 분석하여 문제 해결을 위한 기술 서비스를\n기획 및 제작하고 있습니다.")
+                        .font(.system(size: 17, weight: .bold))
+                        .lineSpacing(17 * 1.6 - 17)
+                        .padding(.vertical, (17 * 1.6 - 17) / 2)
                     
-                    TeamOneView(emoji: "🌿", name: "첼란 Celan", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "🙃", name: "레츠 Lets", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "🐠", name: "리앤 Lianne", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "🥐", name: "매들린 Madeline", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "🌏", name: "지구 Jigu", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "🍋", name: "린 Reene", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "👃🏻", name: "코비 Koby", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    TeamOneView(emoji: "☕️", name: "모카 Moca", jobTitle: "iOS Developer", customMessage: "첼란이 하고 싶은 말")
-                    
+                    Spacer()
+                }
+                .padding(.leading, 24)
+                .padding(.bottom, 50)
+                
+                TeamOneView(emoji: "🌿", name: "첼란 Celan", jobTitle: "iOS Developer", customMessage: "EXC_BAD_ACCESS")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                HStack {
+                    Circle()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.BaseBGWhite)
+                        .overlay {
+                            Text("🙃")
+                                .font(.largeTitle)
+                        }
+                    VStack {
+                        HStack {
+                            Text("레츠 Lets")
+                                .font(.headline)
+                                .bold()
+                                .foregroundStyle(Color.GR9)
+                            Spacer()
+                        }
+                        HStack {
+                            Text("iOS Developer")
+                                .font(.footnote)
+                                .fontWeight(.medium)
+                                .foregroundStyle(Color.GR5)
+                            Spacer()
+                        }
+                        HStack {
+                            Text("안녕하세요 천재 개(발자) 레츠입니다 :)")
+                                .rotationEffect(Angle(degrees: 180))
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(Color.GR5)
+                            Spacer()
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.vertical, 16)
+                .padding(.leading, 16)
+                .background(Color.GR1)
+                .cornerRadius(22)
+                .padding(.horizontal, 16)
+                .padding(.bottom, 12)
+                TeamOneView(emoji: "🐠", name: "리앤 Lianne", jobTitle: "iOS Developer", customMessage: "홍콩 마라주먹🥊 개발자 초이리앤")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                TeamOneView(emoji: "🥐", name: "매들린 Madeline", jobTitle: "iOS Developer", customMessage: "do { try 행복 } catch { 야근 }")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                TeamOneView(emoji: "🌏", name: "지구 Jigu", jobTitle: "Product Manager", customMessage: "방금 얘기한 내용 노션에 정리되어있습니다~! :)")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                TeamOneView(emoji: "🍋", name: "린 Reene", jobTitle: "Product Manager", customMessage: "호방하지만 기품있는 외조담당")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                TeamOneView(emoji: "👃🏻", name: "코비 Koby", jobTitle: "UXUI Designer", customMessage: "좋은 제품을 잘 만들고 싶은 디자이너")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                TeamOneView(emoji: "☕️", name: "모카 Moca", jobTitle: "UXUI Designer", customMessage: "우주? 아~ 제가 발로 밟고 있는거요?")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 12)
+                
+            }
+        }
+        .navigationTitle("만든 사람들")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                            .bold()
+                        Text("설정")
+                            .font(.system(size: 17))
+                    }
+                    .tint(Color.OR5)
                 }
             }
-            .navigationTitle("만든 사람들")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
@@ -84,6 +153,7 @@ struct TeamOneView: View {
                     Spacer()
                 }
             }
+            .padding(.leading, 12)
             Spacer()
         }
         .padding(.vertical, 16)
@@ -91,8 +161,4 @@ struct TeamOneView: View {
         .background(Color.GR1)
         .cornerRadius(22)
     }
-}
-
-#Preview {
-    SettingsTeamView()
 }
