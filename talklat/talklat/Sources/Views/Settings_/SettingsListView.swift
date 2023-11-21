75b873238b9fb4af568199d154f76b58eacc31a5
MADElinessss <jyseen@naver.com>
2023-11-21 16:38:48 +0900

    [Fix/#164] settinglistview padding 조정

--- talklat/talklat/Sources/Views/Settings_/SettingsListView.swift
+++ talklat/talklat/Sources/Views/Settings_/SettingsListView.swift
@@ -83,6 +83,7 @@ struct SettingsListView: View {
                     Text(category)
                         .foregroundColor(.GR8)
                         .font(.system(size: 20, weight: .bold))
+                        .padding(.leading, 8)
                     
                     // Each Setting Cell
                     ForEach(SectionType.allCases, id: \.rawValue) { item in
@@ -135,6 +136,7 @@ struct SettingsListView: View {
             }
         }
         .padding(.horizontal, 16)
+        .padding(.top, 20)
         .navigationTitle("설정")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
