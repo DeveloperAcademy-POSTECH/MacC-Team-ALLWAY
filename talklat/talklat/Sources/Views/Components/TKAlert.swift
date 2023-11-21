bf373b1de78e12b2b8f455da601c120ad30b42d9
valselee <sollleky72@gmail.com>
2023-11-21 16:19:46 +0900

    [Update/#138] 1. TKAlert 관련 백그라운드 버그 해결... 2. TKAlert 쪽의 description 수정

--- talklat/talklat/Sources/Views/Components/TKAlert.swift
+++ talklat/talklat/Sources/Views/Components/TKAlert.swift
@@ -144,7 +144,7 @@ extension TKAlert {
         case .mic:
             """
             비스담을 이용하기 위해
-            마이크 접근 권한을 허용해 주세요.
+            마이크와 음성 인식 접근 권한을 허용해 주세요.
             """
         case .cancellation:
             """
