27b8f6399b4eb15593d63c1ace5facb17bb77940
valselee <sollleky72@gmail.com>
2023-11-23 16:29:45 +0900

    [Add/#184] 1. View+TKAlert 에서 showTKAlert 를 처리하는 메소드 구현(onDismiss에 따라 구별)

---
+++ talklat/talklat/Sources/Utilities/Extensions/View+TKAlert.swift
@@ -0,0 +1,18 @@
+//
+//  View+TKAlert.swift
+//  bisdam
+//
+//  Created by Celan on 11/23/23.
+//
+
+import SwiftUI
+
+struct View_TKAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_TKAlert()
+}
