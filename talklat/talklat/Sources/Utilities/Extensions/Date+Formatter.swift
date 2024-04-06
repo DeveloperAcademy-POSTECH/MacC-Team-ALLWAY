eb7bcffa33653a2b66b880398a4cfaf3f0a30fc0
MADElinessss <jyseen@naver.com>
2024-04-07 03:33:57 +0900

    [Feat/#234] Date Format 분기처리

--- talklat/talklat/Sources/Utilities/Extensions/Date+Formatter.swift
+++ talklat/talklat/Sources/Utilities/Extensions/Date+Formatter.swift
@@ -11,22 +11,20 @@ extension Date {
     public func convertToDate() -> String {
         let currentLocale = Locale.preferredLanguages.first ?? "Language not found"
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy.MM.dd (E)"
-        dateFormatter.locale = Locale(
-            identifier: currentLocale == "en" ? "en" : "ko_KR"
-        )
+        dateFormatter.dateFormat = currentLocale == "en" ? "MMMM d, yyyy" : "yyyy.MM.dd (E)"
+        dateFormatter.locale = Locale(identifier: currentLocale == "en" ? "en_US" : "ko_KR")
+                
         let convertedDate = dateFormatter.string(from: self)
         return convertedDate
     }
     
     public func convertToTime() -> String {
         let currentLocale = Locale.preferredLanguages.first ?? "Language not found"
-        let timeFormmatter = DateFormatter()
-        timeFormmatter.dateFormat = "a hh:mm"
-        timeFormmatter.locale = Locale(
-            identifier: currentLocale == "en" ? "en" : "ko_KR"
-        )
-        let convertedTime = timeFormmatter.string(from: self)
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = currentLocale == "en" ? "h:mm a" : "a hh:mm"
+        timeFormatter.locale = Locale(identifier: currentLocale == "en" ? "en_US" : "ko_KR")
+                
+        let convertedTime = timeFormatter.string(from: self)
         return convertedTime
     }
 }
