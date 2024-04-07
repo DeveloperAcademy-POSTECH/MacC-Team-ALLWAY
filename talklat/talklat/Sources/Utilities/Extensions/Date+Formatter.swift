b9f85a913360ed1504f1cece34e4b7f2628c6e23
MADElinessss <jyseen@naver.com>
2024-04-07 21:31:05 +0900

    [Feat/#234] Date, Time format - current locale 가져오는 로직 삭제

--- talklat/talklat/Sources/Utilities/Extensions/Date+Formatter.swift
+++ talklat/talklat/Sources/Utilities/Extensions/Date+Formatter.swift
@@ -9,22 +9,21 @@ import Foundation
 
 extension Date {
     public func convertToDate() -> String {
-        let currentLocale = Locale.preferredLanguages.first ?? "Language not found"
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = currentLocale == "en" ? "MMMM d, yyyy" : "yyyy.MM.dd (E)"
-        dateFormatter.locale = Locale(identifier: currentLocale == "en" ? "en_US" : "ko_KR")
-                
-        let convertedDate = dateFormatter.string(from: self)
-        return convertedDate
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale.current
+        
+        return dateFormatter.string(from: self)
     }
     
+    
     public func convertToTime() -> String {
-        let currentLocale = Locale.preferredLanguages.first ?? "Language not found"
         let timeFormatter = DateFormatter()
-        timeFormatter.dateFormat = currentLocale == "en" ? "h:mm a" : "a hh:mm"
-        timeFormatter.locale = Locale(identifier: currentLocale == "en" ? "en_US" : "ko_KR")
-                
-        let convertedTime = timeFormatter.string(from: self)
-        return convertedTime
+        timeFormatter.dateStyle = .none
+        timeFormatter.timeStyle = .short
+        timeFormatter.locale = Locale.current
+        
+        return timeFormatter.string(from: self)
     }
 }
