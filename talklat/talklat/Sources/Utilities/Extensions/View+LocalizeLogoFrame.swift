c16e7dada3e5e27e8e5ca2710b4543a9270a8ab7
valselee <sollleky72@gmail.com>
2024-04-10 23:22:40 +0900

    [Update/#246] 1. Lottie 의존성 삭제(마침내) 2. 영문 사용자를 위한 Logo 추가 3. splash를 Icon만 보여주도록 업데이트

--- talklat/talklat/Sources/Utilities/Extensions/View+LocalizeLogoFrame.swift
+++ talklat/talklat/Sources/Utilities/Extensions/View+LocalizeLogoFrame.swift
@@ -5,4 +5,18 @@
 //  Created by Celan on 4/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+  @ViewBuilder
+  func localizeLogoFrame() -> some View {
+    switch Locale.autoupdatingCurrent.identifier {
+    case "en_US_POSIX":
+      self.frame(width: 88, height: 16.6)
+    case "ko_KR":
+      self.frame(width: 57.4, height: 24)
+    default:
+      self.frame(width: 88, height: 16.6)
+    }
+  }
+}
--- talklat/talklat/talklatApp.swift
+++ talklat/talklat/talklatApp.swift
@@ -5,7 +5,7 @@
 //  Created by Celan on 2023/10/05.
 //
 //
-import Lottie
+
 import SwiftUI
 import SwiftData
 
@@ -17,20 +17,17 @@ struct talklatApp: App {
     @StateObject private var colorSchemeManager = ColorSchemeManager()
     
     @State private var swiftDataManager: TKSwiftDataStore = TKSwiftDataStore()
-    @State private var lottiePlaybackMode: LottiePlaybackMode = LottiePlaybackMode.paused
     
     var body: some Scene {
         WindowGroup {
             Group {
                 if case .splash = authManager.authStatus {
-                    TKSplashView(playbackMode: $lottiePlaybackMode)
-                        .transition(.opacity.animation(.easeInOut))
-                        .onChange(of: lottiePlaybackMode) { _, newValue in
-                            if newValue == .paused {
-                                Task { try? await Task.sleep(for: .seconds(0.3)) }
-                                authManager.checkOnboardingCompletion()
-                            }
-                        }
+                    TKSplashView()
+                          .transition(.opacity.animation(.easeInOut))
+                          .task {
+                              try? await Task.sleep(for: .seconds(0.3))
+                              authManager.checkOnboardingCompletion()
+                          }
                 }
                 
                 if case .onboarding = authManager.authStatus {
