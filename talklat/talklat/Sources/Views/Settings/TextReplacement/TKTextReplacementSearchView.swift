90fa593a3e6b6a15b64322e937b8184c91e96fe5
MADElinessss <jyseen@naver.com>
2024-05-12 12:38:37 +0900

    [Feat/#230] 타이핑 전 포커스 상태에서는 텅뷰 에셋 삭제

--- talklat/talklat/Sources/Views/Settings/TextReplacement/TKTextReplacementSearchView.swift
+++ talklat/talklat/Sources/Views/Settings/TextReplacement/TKTextReplacementSearchView.swift
@@ -35,7 +35,7 @@ struct TKTextReplacementSearchView: View {
     }
 
     var body: some View {
-        if filteredLists.isEmpty {
+        if filteredLists.isEmpty && !store(\.searchText).isEmpty {
             emptySearchResultView
         } else {
             ScrollView {
@@ -49,7 +49,7 @@ struct TKTextReplacementSearchView: View {
             Spacer()
             Image(colorScheme == .light ? "search.result.none.light" : "search.result.none.dark")
                 .padding(.bottom, 40)
-            BDText(text: "검색 결과가 없어요", style: .H1_B_130)
+            BDText(text: "검색 결과가 없어요", style: .H1_SB_130)
                 .foregroundColor(Color.GR3)
             Spacer()
         }
