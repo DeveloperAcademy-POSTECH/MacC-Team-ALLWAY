3651fa477e7c8b30c04225595dc16d4eb6e4b683
MADElinessss <jyseen@naver.com>
2025-04-25 00:53:16 +0900

    [FIX/#278] 1. LineHeight 들쭉날쭉 -> G타입130%/C타입170% 통일

--- talklat/talklat/Sources/Views/Components/DesignSystem/BDText.swift
+++ talklat/talklat/Sources/Views/Components/DesignSystem/BDText.swift
@@ -61,33 +61,34 @@ struct BDText: View {
     
     private func propertiesForStyle(_ style: BDTextStyle) -> (Font, Font.Weight, CGFloat, CGFloat) {
         switch style {
+            // MARK: G-Type
             // Large Title Styles
         case .LT_B_120:
-            return (Font.custom("Pretendard-Bold", size: 34), .bold, 1.2, 34)
+            return (Font.custom("Pretendard-Bold", size: 34), .bold, 1.3, 34)
         case .LT_SB_120:
-            return (Font.custom("Pretendard-SemiBold", size: 34), .semibold, 1.2, 34)
+            return (Font.custom("Pretendard-SemiBold", size: 34), .semibold, 1.3, 34)
         case .LT_M_120:
-            return (Font.custom("Pretendard-Medium", size: 34), .medium, 1.2, 34)
+            return (Font.custom("Pretendard-Medium", size: 34), .medium, 1.3, 34)
             
             // Title Styles
         case .T1_B_120:
-            return (Font.custom("Pretendard-Bold", size: 28), .bold, 1.2, 28)
+            return (Font.custom("Pretendard-Bold", size: 28), .bold, 1.3, 28)
         case .T1_SB_120:
-            return (Font.custom("Pretendard-SemiBold", size: 28), .semibold, 1.2, 28)
+            return (Font.custom("Pretendard-SemiBold", size: 28), .semibold, 1.3, 28)
         case .T1_M_120:
-            return (Font.custom("Pretendard-Medium", size: 28), .medium, 1.2, 28)
+            return (Font.custom("Pretendard-Medium", size: 28), .medium, 1.3, 28)
         case .T2_B_125:
-            return (Font.custom("Pretendard-Bold", size: 22), .bold, 1.25, 22)
+            return (Font.custom("Pretendard-Bold", size: 22), .bold, 1.3, 22)
         case .T2_SB_125:
-            return (Font.custom("Pretendard-SemiBold", size: 22), .semibold, 1.25, 22)
+            return (Font.custom("Pretendard-SemiBold", size: 22), .semibold, 1.3, 22)
         case .T2_M_125:
-            return (Font.custom("Pretendard-Medium", size: 22), .medium, 1.25, 22)
+            return (Font.custom("Pretendard-Medium", size: 22), .medium, 1.3, 22)
         case .T3_B_125:
-            return (Font.custom("Pretendard-Bold", size: 20), .bold, 1.25, 20)
+            return (Font.custom("Pretendard-Bold", size: 20), .bold, 1.3, 20)
         case .T3_SB_125:
-            return (Font.custom("Pretendard-SemiBold", size: 20), .semibold, 1.25, 20)
+            return (Font.custom("Pretendard-SemiBold", size: 20), .semibold, 1.3, 20)
         case .T3_M_125:
-            return (Font.custom("Pretendard-Medium", size: 20), .medium, 1.25, 20)
+            return (Font.custom("Pretendard-Medium", size: 20), .medium, 1.3, 20)
             
             // Headline Styles
         case .H1_B_130:
@@ -97,15 +98,15 @@ struct BDText: View {
         case .H1_M_130:
             return (Font.custom("Pretendard-Medium", size: 17), .medium, 1.3, 17)
         case .H2_SB_135:
-            return (Font.custom("Pretendard-SemiBold", size: 15), .semibold, 1.35, 15)
+            return (Font.custom("Pretendard-SemiBold", size: 15), .semibold, 1.3, 15)
         case .H2_M_135:
-            return (Font.custom("Pretendard-Medium", size: 15), .medium, 1.35, 15)
+            return (Font.custom("Pretendard-Medium", size: 15), .medium, 1.3, 15)
             
             // Footnote Styles
         case .FN_SB_135:
-            return (Font.custom("Pretendard-SemiBold", size: 13), .semibold, 1.35, 13)
+            return (Font.custom("Pretendard-SemiBold", size: 13), .semibold, 1.3, 13)
         case .FN_M_135:
-            return (Font.custom("Pretendard-Medium", size: 13), .medium, 1.35, 13)
+            return (Font.custom("Pretendard-Medium", size: 13), .medium, 1.3, 13)
             
             // Caption Styles
         case .C1_SB_130:
@@ -113,39 +114,39 @@ struct BDText: View {
         case .C1_M_130:
             return (Font.custom("Pretendard-Medium", size: 12), .medium, 1.3, 12)
         case .C2_SB_120:
-            return (Font.custom("Pretendard-SemiBold", size: 11), .semibold, 1.2, 11)
+            return (Font.custom("Pretendard-SemiBold", size: 11), .semibold, 1.3, 11)
             
-            // Conversation+HistoryBubble Type (for C)
+            // MARK: C-Type
             // Large Title Styles
         case .LT_B_160:
-            return (Font.custom("Pretendard-Bold", size: 34), .bold, 1.6, 34)
+            return (Font.custom("Pretendard-Bold", size: 34), .bold, 1.7, 34)
         case .LT_M_160:
-            return (Font.custom("Pretendard-Medium", size: 34), .medium, 1.6, 34)
+            return (Font.custom("Pretendard-Medium", size: 34), .medium, 1.7, 34)
             
             
             // Title Styles
         case .T1_B_170:
-            return (Font.custom("Pretendard-Bold", size: 28), .bold, 1.45, 28) // 피그마에 비해 행간 1.7 너무 넓어요ㅠㅠ
+            return (Font.custom("Pretendard-Bold", size: 28), .bold, 1.7, 28) // 피그마에 비해 행간 1.7 너무 넓어요ㅠㅠ
         case .T1_M_170:
             return (Font.custom("Pretendard-Medium", size: 28), .medium, 1.7, 28)
         case .T2_B_160:
-            return (Font.custom("Pretendard-Bold", size: 22), .bold, 1.6, 22)
+            return (Font.custom("Pretendard-Bold", size: 22), .bold, 1.7, 22)
         case .T2_M_160:
-            return (Font.custom("Pretendard-Medium", size: 22), .medium, 1.6, 22)
+            return (Font.custom("Pretendard-Medium", size: 22), .medium, 1.7, 22)
         case .T3_B_160:
-            return (Font.custom("Pretendard-Bold", size: 20), .bold, 1.4, 20)
+            return (Font.custom("Pretendard-Bold", size: 20), .bold, 1.7, 20)
         case .T3_M_160:
-            return (Font.custom("Pretendard-Medium", size: 20), .medium, 1.6, 20)
+            return (Font.custom("Pretendard-Medium", size: 20), .medium, 1.7, 20)
             
             // Headline Styles
         case .H1_B_160:
-            return (Font.custom("Pretendard-Bold", size: 17), .bold, 1.6, 17)
+            return (Font.custom("Pretendard-Bold", size: 17), .bold, 1.7, 17)
         case .H1_M_160:
-            return (Font.custom("Pretendard-Medium", size: 17), .medium, 1.6, 17)
+            return (Font.custom("Pretendard-Medium", size: 17), .medium, 1.7, 17)
         case .H2_SB_160:
-            return (Font.custom("Pretendard-SemiBold", size: 15), .semibold, 1.6, 15)
+            return (Font.custom("Pretendard-SemiBold", size: 15), .semibold, 1.7, 15)
         case .H2_M_160:
-            return (Font.custom("Pretendard-Medium", size: 15), .medium, 1.6, 15)
+            return (Font.custom("Pretendard-Medium", size: 15), .medium, 1.7, 15)
             
         }
     }
